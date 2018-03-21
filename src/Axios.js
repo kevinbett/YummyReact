bd016e36a1e683c465489668839434984a916bd0
kevinsamoei <samoeikev@gmail.com>
2018-03-21 20:24:14 +0300

    [Fix] Add heroku url

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/',
+  baseURL: 'https://yummyrecipesapi.herokuapp.com/api/',
   headers: {'x-access-token': `${localStorage.getItem('token')}` },
 });
 
